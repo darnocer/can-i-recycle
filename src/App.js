68b9e66d54a772a1eba5892e55d729bc2bf68af2
darnocer <darnocer@gmail.com>
2021-07-24 10:32:53 -0600

    working search and filter with recycle data

--- src/App.js
+++ src/App.js
@@ -1,144 +1,59 @@
 import React, { useEffect, useState } from "react";
-// Note: the empty deps array [] means
-// this useEffect will run once
+import axios from "axios";
+
 function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-  //     set search query to empty string
-  const [q, setQ] = useState("");
-  //     set search parameters
-  //     we only what to search countries by capital and name
-  //     this list can be longer if you want
-  //     you can search countries even by their population
-  // just add it to this array
-  const [searchParam] = useState(["capital", "name"]);
-  //     add a default value to be used by our select element
-  const [filterParam, setFilterParam] = useState(["All"]);
+  const [allData, setAllData] = useState([]);
+  const [filteredData, setFilteredData] = useState(allData);
 
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
+  const styles = {
+    display: "inline",
+    width: "30%",
+    height: 50,
+    float: "left",
+    padding: 5,
+    border: "0.5px solid black",
+    marginBottom: 10,
+    marginRight: 10,
+  };
+
   useEffect(() => {
-    fetch("https://restcountries.eu/rest/v2/all")
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+    axios("./data.json")
+      .then((response) => {
+        console.log(response.data);
+        setAllData(response.data);
+        setFilteredData(response.data);
+      })
+      .catch((error) => {
+        console.log("Error getting fake data: " + error);
+      });
   }, []);
 
-  /* here we create a function 
-//     we filter the items
-// use array property .some() to return an item even if other requirements didn't match
-  */
-  function search(items) {
-    return items.filter((item) => {
-      /* 
-//             in here we check if our region is equal to our c state
-// if it's equal to then only return the items that match
-// if not return All the countries
-          */
-      if (item.region == filterParam) {
-        return searchParam.some((newItem) => {
-          return (
-            item[newItem].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
-          );
-        });
-      } else if (filterParam == "All") {
-        return searchParam.some((newItem) => {
-          return (
-            item[newItem].toString().toLowerCase().indexOf(q.toLowerCase()) > -1
-          );
-        });
-      }
+  const handleSearch = (event) => {
+    let value = event.target.value.toLowerCase();
+    let result = [];
+    console.log(value);
+    result = allData.filter((data) => {
+      return data.keywords.search(value) != -1;
     });
-  }
+    setFilteredData(result);
+  };
 
-  if (error) {
-    return <>{error.message}</>;
-  } else if (!isLoaded) {
-    return <>loading...</>;
-  } else {
-    return (
-      <div className="wrapper">
-        <div className="search-wrapper">
-          <label htmlFor="search-form">
-            <input
-              type="search"
-              name="search-form"
-              id="search-form"
-              className="search-input"
-              placeholder="Search for..."
-              value={q}
-              /* 
-                          // set the value of our useState e
-                          //  anytime the user types in the search box
-                          */
-              onChange={(e) => setQ(e.target.value)}
-            />
-            <span className="sr-only">Search countries here</span>
-          </label>
-
-          <div className="select">
-            <select
-              /* 
-//                         here we create a basic select input
-//                     we set the value to the selected value 
-//                     and update the setC() state every time onChange is called
-                  */
-              onChange={(e) => {
-                setFilterParam(e.target.value);
-              }}
-              className="custom-select"
-              aria-label="Filter Countries By Countries">
-              <option value="All">Filter By Region</option>
-              <option value="Africa">Africa</option>
-              <option value="Americas">America</option>
-              <option value="Asia">Asia</option>
-              <option value="Europe">Europe</option>
-              <option value="Oceania">Oceania</option>
-            </select>
-            <span className="focus"></span>
-          </div>
-        </div>
-        <ul className="card-grid">
-          {search(items).map((item) => (
-            <li>
-              <article className="card" key={item.id}>
-                <div className="card-image">
-                  <img src={item.flag} alt={item.name} />
-                </div>
-                <div className="card-content">
-                  <h2 className="card-name">{item.name}</h2>
-                  <ol className="card-list">
-                    <li>
-                      population: <span>{item.population}</span>
-                    </li>
-                    <li>
-                      Region: <span>{item.region}</span>
-                    </li>
-                    <li>
-                      Capital: <span>{item.capital}</span>
-                    </li>
-                  </ol>
-                </div>
-              </article>
-            </li>
-          ))}
-        </ul>
+  return (
+    <div className="App">
+      <div style={{ margin: "0 auto", marginTop: "10%" }}>
+        <label>Search:</label>
+        <input type="text" onChange={(event) => handleSearch(event)} />
       </div>
-    );
-  }
+      <div style={{ padding: 10 }}>
+        {filteredData.map((value, index) => {
+          return (
+            <div key={value.name}>
+              <div style={styles}>{value.name}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 export default App;
